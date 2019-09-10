aff4c062f53cb16b79166ba90732c98c60530427
vincentnqc <vincentnqc92@gmail.com>
2019-09-09 20:23:30 -0500

    first commit

---
+++ 04week/loop.js
@@ -0,0 +1,44 @@
+let range = 0;
+// do {
+//   range += 1;
+//   console.log(range);
+// };
+
+let person = {
+  
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+};
+
+let person = {
+  
+  firstName: "John",
+  lastName: "Doe",
+  birthDate: "Feb 6, 1975",
+  gender: "male"
+};
+
+let person = {
+  
+  firstName: "Dave",
+  lastName: "Doe",
+  birthDate: "Mar 28, 1985",
+  gender: "male"
+};
+
+let person = {
+  
+  firstName: "Alex",
+  lastName: "Doe",
+  birthDate: "Oct 5, 1995",
+  gender: "male"
+};
+
+for (let i = 1; i < person['birthDate'].length; i++){
+  if (person['birthDate'].split) (' ') [1] % 2 != 0;
+
+  console.log(person['birthDate'].split (' ')[i]);
+}
+
