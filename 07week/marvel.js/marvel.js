e2e14ae6e4baaa02d78577d5b0c9e5dffd8a4a43
vincentnqc <vincentnqc92@gmail.com>
2019-10-03 14:01:10 -0500

    heros

--- 07week/marvel.js/marvel.js
+++ 07week/marvel.js/marvel.js
@@ -1,1 +1,25 @@
-requestAnimationFrame('dotenv').config()
+// requestAnimationFrame('dotenv').config()
+
+var heros;
+
+window.onload = function() {
+  gitPosts()
+}
+
+const gitPosts = () => {
+  fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c5893bf85cfc66cc3e579b13dee23b04&hash=eab615c7f471a52fa4bff55f46731a81")
+  .then(res => res.json())
+  .then(posts =>{
+    console.log(posts)
+    heros = (posts.data)
+    display()
+  })
+}
+
+
+
+const display = () => {
+console.log(heros)
+document.getElementById("herolist").append(document.createTextNode(heros.results[0].characters.items[0].name))
+}
+
